d3e8f5fe104fe9cb0892fce019ac873e08317d7a
vibqetowi <abutcher.usa@protonmail.com>
2024-11-15 15:52:55 -0500

    update ddl

--- Implementation/Persistence/DDL-342-project.sql
+++ Implementation/Persistence/DDL-342-project.sql
@@ -1,23 +1,3 @@
-CREATE TABLE "permissions" (
-  "permission_id" char(36) PRIMARY KEY,
-  "name" varchar UNIQUE,
-  "action_type" varchar
-);
-
-CREATE TABLE "user_permissions" (
-  "permission_id" char(36),
-  "table_name" varchar,
-  "record_id" char(36),
-  PRIMARY KEY ("permission_id", "table_name", "record_id")
-);
-
-CREATE TABLE "resource_permissions" (
-  "permission_id" char(36),
-  "table_name" varchar,
-  "record_id" char(36),
-  PRIMARY KEY ("permission_id", "table_name", "record_id")
-);
-
 CREATE TABLE "provinces" (
   "location_id" char(36) PRIMARY KEY,
   "name" varchar
@@ -115,131 +95,46 @@ CREATE TABLE "bookings" (
   PRIMARY KEY ("booked_by_client_id", "public_offering_id")
 );
 
-CREATE TABLE "audit_logs" (
-  "log_id" char(36) PRIMARY KEY,
-  "timestamp" timestamp,
-  "table_name" varchar,
-  "actor_id" char(36),
-  "action_type" varchar,
-  "target_table" varchar,
-  "record_id" char(36),
-  "old_value" json,
-  "new_value" json,
-  "metadata" json
-);
-
 CREATE INDEX ON "schedules" ("owner_id", "owner_type");
 
-ALTER TABLE "user_permissions"
-ADD FOREIGN KEY ("permission_id")
-REFERENCES "permissions" ("permission_id")
-ON DELETE CASCADE;
+ALTER TABLE "cities" ADD FOREIGN KEY ("parent_location_id") REFERENCES "provinces" ("location_id") ON DELETE CASCADE;
 
-ALTER TABLE "resource_permissions"
-ADD FOREIGN KEY ("permission_id")
-REFERENCES "permissions" ("permission_id")
-ON DELETE CASCADE;
+ALTER TABLE "branches" ADD FOREIGN KEY ("parent_location_id") REFERENCES "cities" ("location_id") ON DELETE CASCADE;
 
-ALTER TABLE "cities"
-ADD FOREIGN KEY ("parent_location_id")
-REFERENCES "provinces" ("location_id")
-ON DELETE CASCADE;
+ALTER TABLE "schedules" ADD FOREIGN KEY ("schedule_id") REFERENCES "branches" ("schedule_id") ON DELETE CASCADE;
 
-ALTER TABLE "branches"
-ADD FOREIGN KEY ("parent_location_id")
-REFERENCES "cities" ("location_id")
-ON DELETE CASCADE;
+ALTER TABLE "schedules" ADD FOREIGN KEY ("schedule_id") REFERENCES "instructors" ("schedule_id") ON DELETE CASCADE;
 
-ALTER TABLE "schedules"
-ADD FOREIGN KEY ("schedule_id")
-REFERENCES "branches" ("schedule_id")
-ON DELETE CASCADE;
+ALTER TABLE "schedules" ADD FOREIGN KEY ("schedule_id") REFERENCES "clients" ("schedule_id") ON DELETE CASCADE;
 
-ALTER TABLE "schedules"
-ADD FOREIGN KEY ("schedule_id")
-REFERENCES "instructors" ("schedule_id")
-ON DELETE CASCADE;
+ALTER TABLE "clients" ADD FOREIGN KEY ("user_id") REFERENCES "clients" ("guardian_id");
 
-ALTER TABLE "schedules"
-ADD FOREIGN KEY ("schedule_id")
-REFERENCES "clients" ("schedule_id")
-ON DELETE CASCADE;
+ALTER TABLE "time_slots" ADD FOREIGN KEY ("schedule_id") REFERENCES "schedules" ("schedule_id") ON DELETE CASCADE;
 
-ALTER TABLE "clients"
-ADD FOREIGN KEY ("user_id")
-REFERENCES "clients" ("guardian_id");
+ALTER TABLE "time_slots" ADD FOREIGN KEY ("reserved_by_public_offering_id") REFERENCES "public_offerings" ("offering_id") ON DELETE CASCADE;
 
-ALTER TABLE "time_slots"
-ADD FOREIGN KEY ("schedule_id")
-REFERENCES "schedules" ("schedule_id")
-ON DELETE CASCADE;
+ALTER TABLE "instructors" ADD FOREIGN KEY ("schedule_id") REFERENCES "schedules" ("owner_id") ON DELETE CASCADE;
 
-ALTER TABLE "time_slots"
-ADD FOREIGN KEY ("reserved_by_public_offering_id")
-REFERENCES "public_offerings" ("offering_id")
-ON DELETE CASCADE;
+ALTER TABLE "clients" ADD FOREIGN KEY ("schedule_id") REFERENCES "schedules" ("owner_id") ON DELETE CASCADE;
 
-ALTER TABLE "instructors"
-ADD FOREIGN KEY ("schedule_id")
-REFERENCES "schedules" ("owner_id")
-ON DELETE CASCADE;
+ALTER TABLE "branches" ADD FOREIGN KEY ("schedule_id") REFERENCES "schedules" ("owner_id") ON DELETE CASCADE;
 
-ALTER TABLE "clients"
-ADD FOREIGN KEY ("schedule_id")
-REFERENCES "schedules" ("owner_id")
-ON DELETE CASCADE;
+ALTER TABLE "offerings" ADD FOREIGN KEY ("offering_id") REFERENCES "public_offerings" ("offering_id") ON DELETE CASCADE;
 
-ALTER TABLE "branches"
-ADD FOREIGN KEY ("schedule_id")
-REFERENCES "schedules" ("owner_id")
-ON DELETE CASCADE;
+ALTER TABLE "public_offerings" ADD FOREIGN KEY ("instructor_id") REFERENCES "instructors" ("user_id") ON DELETE CASCADE;
 
-ALTER TABLE "offerings"
-ADD FOREIGN KEY ("offering_id")
-REFERENCES "public_offerings" ("offering_id")
-ON DELETE CASCADE;
+ALTER TABLE "schedules" ADD FOREIGN KEY ("schedule_id") REFERENCES "public_offerings" ("schedule_id") ON DELETE CASCADE;
 
-ALTER TABLE "public_offerings"
-ADD FOREIGN KEY ("instructor_id")
-REFERENCES "instructors" ("user_id")
-ON DELETE CASCADE;
+ALTER TABLE "instructor_branch_availability" ADD FOREIGN KEY ("instructor_id") REFERENCES "instructors" ("user_id") ON DELETE CASCADE;
 
-ALTER TABLE "schedules"
-ADD FOREIGN KEY ("schedule_id")
-REFERENCES "public_offerings" ("schedule_id")
-ON DELETE CASCADE;
+ALTER TABLE "instructor_branch_availability" ADD FOREIGN KEY ("branch_id") REFERENCES "branches" ("location_id") ON DELETE CASCADE;
 
-ALTER TABLE "instructor_branch_availability"
-ADD FOREIGN KEY ("instructor_id")
-REFERENCES "instructors" ("user_id")
-ON DELETE CASCADE;
+ALTER TABLE "offered_in_branch" ADD FOREIGN KEY ("offering_id") REFERENCES "offerings" ("offering_id") ON DELETE CASCADE;
 
-ALTER TABLE "instructor_branch_availability"
-ADD FOREIGN KEY ("branch_id")
-REFERENCES "branches" ("location_id")
-ON DELETE CASCADE;
+ALTER TABLE "offered_in_branch" ADD FOREIGN KEY ("branch_id") REFERENCES "branches" ("location_id") ON DELETE CASCADE;
 
-ALTER TABLE "offered_in_branch"
-ADD FOREIGN KEY ("offering_id")
-REFERENCES "offerings" ("offering_id")
-ON DELETE CASCADE;
+ALTER TABLE "bookings" ADD FOREIGN KEY ("booked_by_client_id") REFERENCES "clients" ("user_id") ON DELETE CASCADE;
 
-ALTER TABLE "offered_in_branch"
-ADD FOREIGN KEY ("branch_id")
-REFERENCES "branches" ("location_id")
-ON DELETE CASCADE;
+ALTER TABLE "bookings" ADD FOREIGN KEY ("public_offering_id") REFERENCES "public_offerings" ("offering_id") ON DELETE CASCADE;
 
-ALTER TABLE "bookings"
-ADD FOREIGN KEY ("booked_by_client_id")
-REFERENCES "clients" ("user_id")
-ON DELETE CASCADE;
-
-ALTER TABLE "bookings"
-ADD FOREIGN KEY ("public_offering_id")
-REFERENCES "public_offerings" ("offering_id")
-ON DELETE CASCADE;
-
-ALTER TABLE "bookings"
-ADD FOREIGN KEY ("booked_for_client_id")
-REFERENCES "clients" ("user_id")
-ON DELETE CASCADE;
+ALTER TABLE "bookings" ADD FOREIGN KEY ("booked_for_client_id") REFERENCES "clients" ("user_id") ON DELETE CASCADE;
